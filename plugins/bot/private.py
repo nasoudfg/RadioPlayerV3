5d4416fe21a7aad68f82f6e5284b9d12df1deecc
nasoudfg <108593914+nasoudfg@users.noreply.github.com>
2022-07-11 22:52:01 +0430

    Update private.py

--- plugins/bot/private.py
+++ plugins/bot/private.py
@@ -265,7 +265,7 @@ async def start(client, message):
             ]
             ]
     reply_markup = InlineKeyboardMarkup(buttons)
-    m=await message.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+    m=await message.reply_photo(photo="https://s6.uupload.ir/files/untitled-1_g44s.png", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
     await mp.delete(m)
     await mp.delete(message)
 
@@ -292,7 +292,7 @@ async def help(client, message):
     reply_markup = InlineKeyboardMarkup(buttons)
     if msg.get('help') is not None:
         await msg['help'].delete()
-    msg['help'] = await message.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption=HELP_TEXT, reply_markup=reply_markup)
+    msg['help'] = await message.reply_photo(photo="https://s6.uupload.ir/files/untitled-1_g44s.png", caption=HELP_TEXT, reply_markup=reply_markup)
     await mp.delete(message)
 
 
